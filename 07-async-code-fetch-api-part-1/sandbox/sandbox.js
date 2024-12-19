afad746ad10da410539f725d0690baba9196944d
ikupryianau <ivan.kupryianau@capital.com>
2024-12-19 16:52:01 +0100

    feat: add fecth data, build class

---
+++ 07-async-code-fetch-api-part-1/sandbox/sandbox.js
@@ -0,0 +1,18 @@
+// TODO How Event Loop works
+
+// while (eventLoop.waitForTask()) {
+//   const taskQueue = eventLoop.selectTaskQueue();
+//   if (taskQueue.hasNextTask()) {
+//     taskQueue.processNextTask()
+//   }
+//
+//   const microtaskQueue = eventLoop.microTaskQueue();
+//   if (microtaskQueue.hasNextMicroTask()) {
+//     microtaskQueue.processNextMicroTask()
+//   }
+//
+//   if (eventLoop.shouldRender()) {
+//     eventLoop.render();
+//   }
+// }
+
